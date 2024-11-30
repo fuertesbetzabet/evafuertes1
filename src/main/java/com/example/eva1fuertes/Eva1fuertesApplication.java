74a6c12a298122a741bdb907f6b4d5f4adc5da19
fuertesbetzabet <u00031041@sise.com.pe>
2024-11-29 21:22:19 -0500

    evafuertes1

---
+++ src/main/java/com/example/eva1fuertes/Eva1fuertesApplication.java
@@ -0,0 +1,13 @@
+package com.example.eva1fuertes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Eva1fuertesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Eva1fuertesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/eva1fuertes/Eva1fuertesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.eva1fuertes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Eva1fuertesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
